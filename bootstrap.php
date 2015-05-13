6eff630986f6e4b378243a7241720f07e73a15de
xemlock <xemlock@gmail.com>
2015-05-13 14:19:53 +0200

    fixed bootstrap

--- bootstrap.php
+++ bootstrap.php
@@ -5,4 +5,6 @@ add_filter('get_twig', function (Twig_Environment $twig) {
     $twig->addFilter(new Twig_SimpleFilter('esc_attr', 'esc_attr'));
 
     $twig->addExtension(new wpToolkitTimber_TwigExtension());
+
+    return $twig;
 });
