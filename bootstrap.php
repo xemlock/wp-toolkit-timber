79c99d05a8dfcfd9c74cc38d7a6b723005ad3286
xemlock <xemlock@gmail.com>
2015-05-13 15:19:27 +0200

    escapers became part of extension

--- bootstrap.php
+++ bootstrap.php
@@ -1,10 +1,6 @@
 <?php
 
 add_filter('get_twig', function (Twig_Environment $twig) {
-    $twig->addFilter(new Twig_SimpleFilter('esc_html', 'esc_html'));
-    $twig->addFilter(new Twig_SimpleFilter('esc_attr', 'esc_attr'));
-
     $twig->addExtension(new wpToolkitTimber_TwigExtension());
-
     return $twig;
 });
--- lib/wpToolkitTimber/TwigExtension.php
+++ lib/wpToolkitTimber/TwigExtension.php
@@ -7,8 +7,26 @@ class wpToolkitTimber_TwigExtension extends Twig_Extension
         return 'wpToolkit';
     }
 
+    public function getFilters()
+    {
+        // Since Timber variables are not automatically escaped provide
+        // build-in Wordpress escapers as filters
+        $filters = array(
+            new Twig_SimpleFilter('esc_attr',     'esc_attr'),
+            new Twig_SimpleFilter('esc_html',     'esc_html'),
+            new Twig_SimpleFilter('esc_js',       'esc_js'),
+            new Twig_SimpleFilter('esc_sql',      'esc_sql'),
+            new Twig_SimpleFilter('esc_textarea', 'esc_textarea'),
+            new Twig_SimpleFilter('esc_url',      'esc_url'),
+        );
+        return $filters;
+    }
+
     public function getGlobals()
     {
+        // this allows more semantic function calls compared to Timber,
+        // instead of {{ function('func', arg1, arg2) }} one can use
+        // {{ function.func(arg1, arg2) }} or {{ fn.func(arg1, arg2) }}
         $fn = new wpToolkitTimber_FunctionCaller();
         return array(
             'fn'       => $fn,
